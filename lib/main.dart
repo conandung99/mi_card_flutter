76c2e0908f36a687ec7bf0af6657a5b13a777ca6
Nguyen Tien Dung/Media Platform P /SVMC/Engineer/Samsung Electronics <dung.nt13@samsung.com>
2020-12-15 18:09:59 +0700

    Card and ListTile & Divider

--- lib/main.dart
+++ lib/main.dart
@@ -28,56 +28,56 @@ class MyApp extends StatelessWidget {
                   style: TextStyle(
                       fontFamily: 'IndieFlower',
                       fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
+                      fontSize: 30.0,
                       color: Colors.blue.shade300),
                 ),
                 Text(
                   'Software Developer',
                   style: TextStyle(
-                    fontSize: 20.0,
+                    fontSize: 30.0,
                     letterSpacing: 3.0,
                     fontWeight: FontWeight.bold,
                     color: Colors.blue.shade900,
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  color: Colors.white,
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.phone_android_rounded,
-                        color: Colors.blue.shade900,
-                      ),
-                      SizedBox(width: 10.0),
-                      Text(
-                        '+84 973 621 000',
-                        style: TextStyle(color: Colors.blue.shade600),
-                      )
-                    ],
+                SizedBox(
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.green.shade50,
+                    height: 20.0,
+                    thickness: 1.0,
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  color: Colors.white,
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.email_rounded,
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone_android_rounded,
                         color: Colors.blue.shade900,
+                        size: 30.0,
                       ),
-                      SizedBox(width: 10.0),
-                      Text(
+                      title: Text(
+                        '+84 973 621 111',
+                        style: TextStyle(color: Colors.blue.shade600),
+                      ),
+                    )),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.mail_outline_outlined,
+                        color: Colors.blue.shade900,
+                        size: 30.0,
+                      ),
+                      title: Text(
                         'tiendungvk.0895@gmail.com',
                         style: TextStyle(color: Colors.blue.shade600),
-                      )
-                    ],
-                  ),
-                )
+                      ),
+                    ))
               ],
             ),
           ))),
