7c5e7fef14c63e735844da9332617c28df4c1a40
Nguyen Tien Dung/Media Platform P /SVMC/Engineer/Samsung Electronics <dung.nt13@samsung.com>
2020-12-15 17:41:05 +0700

    CircleAvatar and TextStyle with new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -10,38 +10,77 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
+          appBar: AppBar(
+            title: Text('User Information'),
+          ),
+          backgroundColor: Colors.greenAccent.shade100,
           body: SafeArea(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 100.0,
-                  color: Colors.red,
+              child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/user-male-200.png'),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.green,
-                    )
-                  ],
+                Text(
+                  'DUNG BK',
+                  style: TextStyle(
+                      fontFamily: 'IndieFlower',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.blue.shade300),
+                ),
+                Text(
+                  'Software Developer',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 3.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue.shade900,
+                  ),
                 ),
                 Container(
-                  width: 100.0,
-                  color: Colors.blue,
+                  padding: EdgeInsets.all(10.0),
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone_android_rounded,
+                        color: Colors.blue.shade900,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        '+84 973 621 000',
+                        style: TextStyle(color: Colors.blue.shade600),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email_rounded,
+                        color: Colors.blue.shade900,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        'tiendungvk.0895@gmail.com',
+                        style: TextStyle(color: Colors.blue.shade600),
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
-          )),
+          ))),
     );
     throw UnimplementedError();
   }
